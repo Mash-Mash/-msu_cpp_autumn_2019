1db33ca91b6883282f73355d6a59212f829513ca
Mash-Mash <masha.logvaneva@gmail.com>
2019-10-18 21:21:48 +0300

    Change calculator.cpp

--- 01/calculator.cpp
+++ 01/calculator.cpp
@@ -13,6 +13,7 @@ digit -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | e
 #include "calculator.h"
 #include "utils.h"
 
+
 std::string getErrorMessage(const char* str)
 {
 	return (*str == '\0' || *str == '\n')
