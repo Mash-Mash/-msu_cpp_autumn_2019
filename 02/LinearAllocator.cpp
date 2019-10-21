00c001cd80c2411a168bd4801ef086b95dd13c53
Mash-Mash <masha.logvaneva@gmail.com>
2019-10-21 18:16:30 +0300

    Change LinearAllocator.cpp

--- 02/LinearAllocator.cpp
+++ 02/LinearAllocator.cpp
@@ -1,15 +1,14 @@
 #include "LinearAllocator.h"
 
-LinearAllocator::LinearAllocator(size_t maxSize) :d_capacity(maxSize), d_size(0)
+LinearAllocator::LinearAllocator(size_t maxSize) :d_capacity(maxSize), d_size(0), d_memory(new char[maxSize])
 {
-	d_memory.reserve(maxSize);
 }
 
 char* LinearAllocator::alloc(size_t size)
 {
 	if (d_size + size <= d_capacity)
 	{
-		char* ptr = d_memory.data() + d_size;
+		char* ptr = d_memory.get() + d_size;
 		d_size += size;
 		return ptr;
 	}
